af24f030aa23ea7c8e6ba659137fcfef467fc3f1
Rodrigo C <rbpdvc@gmail.com>
2018-06-21 22:43:51 +0100

    Added the map component and the necessary changes to the package json to work with it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import the Google Maps API Wrapper from google-maps-react
+import { GoogleApiWrapper } from 'google-maps-react'
+// import child component
+import Map from './Map'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1> Google Maps API + React </h1> 
+        <Map google={this.props.google} />
       </div>
     );
   }
 }
-
-export default App;
+// Here we are exporting the App component WITH the GoogleApiWrapper. Pass it down with an object containing your API key
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAW9FSO3dZau3RhzD1-rHFrVxWyEveq5Os',
+})(App)
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class List extends Component {
+  render() {
+    return(
+      <div id="list-container">
+      </div>
+    )
+  }
+}
+
+export default List
